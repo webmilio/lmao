f30b45e962158d3c8f82dbf46c8fa3cf9179e9f5
Vomis <samusalut@hotmail.com>
2017-01-29 22:21:44 -0500

    Fixed Mortal Strike. Again.
    Another fix for this dumbass ability.

--- scripts/vscripts/heroes/hero_skeleton_king/mortal_strike.lua
+++ scripts/vscripts/heroes/hero_skeleton_king/mortal_strike.lua
@@ -1,15 +1,36 @@
 function MortalStrike( keys )
+	if crit_proc == 1 then
+		local ability = keys.ability
+		local level = ability:GetLevel() - 1
+		local cooldown = ability:GetCooldown(level)
+		local caster = keys.caster	
+		local modifier = keys.modifier
+
+		ability:StartCooldown(cooldown)
+
+		caster:RemoveModifierByName(modifier) 
+
+		Timers:CreateTimer(cooldown, function()
+			ability:ApplyDataDrivenModifier(caster, caster, modifier, {})
+		end)
+		crit_proc = 0
+	end
+end
+
+function CheckAttackStart( keys )
+	local caster = keys.caster
+	local modifier = keys.modifier
+	if caster:HasModifier(modifier) then
+		crit_proc = 1
+	end
+end
+
+function CheckUpgrade( keys )
+	local caster = keys.caster
 	local ability = keys.ability
-	local level = ability:GetLevel() - 1
-	local cooldown = ability:GetCooldown(level)
-	local caster = keys.caster	
-	local modifierName = "modifier_mortal_strike_datadriven"
-
-	ability:StartCooldown(cooldown)
-
-	caster:RemoveModifierByName(modifierName) 
-
-	Timers:CreateTimer(cooldown, function()
-		ability:ApplyDataDrivenModifier(caster, caster, modifierName, {})
-	end)	
+	local level = ability:GetLevel()
+	local modifier = keys.modifier
+	if level == 1 then
+		ability:ApplyDataDrivenModifier(caster, caster, modifier, {})
+	end
 end
